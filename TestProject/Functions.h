395c5a799c6b04d05254a4b07f1fe5b3e663f731
Suleymanov İlkin İlham <Sule_jr92@itstep.edu.az>
2022-03-10 20:25:19 +0400

    I add some Show Functions

---
+++ TestProject/Functions.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+void ShowMenu() {
+	system("cls");
+	cout << endl;
+	cout << "         __ -Choose- __" << endl << endl;
+	cout << "  ____________________________" << endl;
+	cout << endl;
+	cout << " |  1 --> Show All Teachers.. |" << endl;
+	cout << " |  2 --> Create Group..      |" << endl;
+	cout << " |  3 --> Show Groups..       |" << endl;
+	cout << "  ____________________________" << endl;
+}
+
--- TestProject/Source.cpp
+++ TestProject/Source.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include "Functions.h"
 using namespace std;
 
 
 void main() {
-
+	ShowMenu();
 }
